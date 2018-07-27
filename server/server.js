fc00de59c9e9eeff0196cd96abd85fa01007dbaf
ballakkur <ballakkurkarthik@gmail.com>
2018-07-27 10:07:59 +0530

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
 const path = require('path');
 
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname,'../public');
 
 //heroku
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+var io = socketIO(server);
  
 app.use(express.static(publicPath));
-app.get('/',(req,res)=>{
-res.send('hello');
+
+io.on('connection',(socket)=>{
+console.log('user connected');
+
+socket.on('disconnect',()=>{
+    console.log('user was disconnected');
 })
 
-app.listen(3000,()=>{
+});
+
+server.listen(3000,()=>{
 console.log('server running in port',port);
 })
