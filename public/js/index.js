8e427c0e9e53decb63f8ec17163b58e9cd63e587
ballakkur <ballakkurkarthik@gmail.com>
2018-07-27 12:48:43 +0530

    'message-broadcast'

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect',()=>{
     console.log('connected to server');
-    socket.emit('createMessage',{
-        from:'me',
-        text:'hello there'
-    });
 });
 
 socket.on('disconnect',()=>{
@@ -13,6 +9,5 @@ socket.on('disconnect',()=>{
 });
 
 socket.on('newMessage',(data)=>{
-    console.log('new message');
     console.log(`${data.from} say ${data.text}`);
 });
--- server/server.js
+++ server/server.js
@@ -17,14 +17,13 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log('user connected');
 
-socket.emit('newMessage',{
-    from:'mosh',
-    text:'hello',
-    createdAt:123
-});
-
 socket.on('createMessage',(message)=>{
     console.log(message);
+    io.emit('newMessage',{
+        from:message.from,
+        text:message.text,
+        createdAt:new Date().getTime()
+    });
 })
 
 socket.on('disconnect',()=>{
